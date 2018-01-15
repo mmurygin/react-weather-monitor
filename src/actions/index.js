6522b0e83efad988b938ebb25c1bb84d74b66c43
Maksim Murygin <mail@muryginm.com>
2018-01-15 09:04:07 +0700

    correct indent

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,15 +5,15 @@ const API_KEY = '4081184728ad58b84e355a0422f95bef';
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 function getWeatherMapEndpoint(city) {
-    return `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}&q=${city},ru`;
+  return `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}&q=${city},ru`;
 }
 
 export function fetchWeather(city) {
-    const url = getWeatherMapEndpoint(city);
-    const request = axios.get(url);
+  const url = getWeatherMapEndpoint(city);
+  const request = axios.get(url);
 
-    return {
-        type: FETCH_WEATHER,
-        payload: request,
-    };
+  return {
+    type: FETCH_WEATHER,
+    payload: request,
+  };
 }
---
+++ src/containers/weather-list.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class WeatherList extends Component {
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default WeatherList;
